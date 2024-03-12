45f9aa4deca51670b4a47046fde75932c264247a
antonprafanto <anton.prafanto@gmail.com>
2024-03-12 22:49:31 +0800

    Contoh Table Layout sederhana

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,52 +1,38 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Mengakses elemen UI dari layout
-        ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
-        TextView judulTextView = findViewById(R.id.judulTextView);
-        ImageView ikonImageView = findViewById(R.id.ikonImageView);
-        Button tombolButton = findViewById(R.id.tombolButton);
-        progressBar = findViewById(R.id.progressBar);
+        // Mengakses elemen-elemen UI dari layout
+        TableLayout mainTableLayout = findViewById(R.id.mainTableLayout);
+        EditText editTextNama = findViewById(R.id.editTextNama);
+        EditText editTextUsia = findViewById(R.id.editTextUsia);
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
 
         // Menetapkan aksi klik pada tombol
-        tombolButton.setOnClickListener(new View.OnClickListener() {
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Menjalankan tugas yang memakan waktu
-                lakukanTugas();
+                // Mendapatkan nilai dari EditText
+                String nama = editTextNama.getText().toString();
+                String usia = editTextUsia.getText().toString();
+
+                // Menampilkan pesan Toast dengan nilai yang diinputkan
+                Toast.makeText(MainActivity.this, "Nama: " + nama + ", Usia: " + usia, Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private void lakukanTugas() {
-        // Menampilkan ProgressBar
-        progressBar.setVisibility(View.VISIBLE);
-
-        // Menunda tugas selama 2 detik
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Menyembunyikan ProgressBar setelah tugas selesai
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-        }, 2000);
     }
 }
