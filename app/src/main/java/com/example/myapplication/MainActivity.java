ca396614a547ef1d9b276a0fa6a344e9c95921ea
antonprafanto <anton.prafanto@gmail.com>
2024-03-12 22:11:18 +0800

    Contoh Constraint Layout sederhana

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,36 +1,52 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Mengakses elemen-elemen UI dari layout
-        RelativeLayout mainLayout = findViewById(R.id.mainLayout);
-        TextView textView = findViewById(R.id.textView);
-        ImageView imageView = findViewById(R.id.imageView);
-        Button button = findViewById(R.id.button);
-        EditText editText = findViewById(R.id.editText);
+        // Mengakses elemen UI dari layout
+        ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
+        TextView judulTextView = findViewById(R.id.judulTextView);
+        ImageView ikonImageView = findViewById(R.id.ikonImageView);
+        Button tombolButton = findViewById(R.id.tombolButton);
+        progressBar = findViewById(R.id.progressBar);
 
-        // Memberikan aksi klik pada button
-        button.setOnClickListener(new View.OnClickListener() {
+        // Menetapkan aksi klik pada tombol
+        tombolButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Aksi yang ingin dilakukan saat tombol diklik
-                String userInput = editText.getText().toString();
-                textView.setText("Anda memasukkan: " + userInput);
+                // Menjalankan tugas yang memakan waktu
+                lakukanTugas();
             }
         });
+    }
+
+    private void lakukanTugas() {
+        // Menampilkan ProgressBar
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Menunda tugas selama 2 detik
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Menyembunyikan ProgressBar setelah tugas selesai
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        }, 2000);
     }
 }
